b4c9f0b1567d68a569f6bed56efb75d5aa476c68
Mark Miller Foerster <mark.foerster.launchcode@gmail.com>
2019-11-06 23:53:02 -0500

    JS coding started: takeoff button coded-for

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener( 'load', () => {
+    const takeOffButton = document.getElementById( 'takeoff' );
+    takeOffButton.addEventListener( 'click', () => {
+        const takeoffReady = window.confirm(
+            'Confirm that the shuttle is ready for takeoff.' );
+        if ( takeoffReady ) {
+            const flightStatus = document.getElementById( 'flightStatus' );
+            flightStatus.textContent = 'Shuttle in flight.';
+        }
+    })
+})
